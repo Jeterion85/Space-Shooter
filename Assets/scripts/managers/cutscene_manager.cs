88c28de4b96475002e1a8ceb0161c79d936527eb
traba49 <ptrabak@gmail.com>
2021-07-09 21:45:04 +0300

    cutscenes and sprites

---
+++ Assets/scripts/managers/cutscene_manager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutscene_manager : MonoBehaviour
+{
+    public int a;
+
+    public GameObject[] cutscenes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i=0; i<cutscenes.Length; i++)
+        {
+            cutscenes[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (a == 1)
+        {
+            cutscenes[0].SetActive(true);
+        }
+        if (a ==2)
+        {
+            cutscenes[1].SetActive(true);
+        }
+    }
+}
