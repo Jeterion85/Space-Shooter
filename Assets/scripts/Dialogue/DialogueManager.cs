945bc9badeaef5afa11dbac0638e7d616ea2cf44
traba49 <ptrabak@gmail.com>
2021-07-13 13:24:01 +0300

    level transition

--- Assets/scripts/Dialogue/DialogueManager.cs
+++ Assets/scripts/Dialogue/DialogueManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
@@ -33,12 +34,16 @@ public class DialogueManager : MonoBehaviour
                 dBox.SetActive(false);
                 dActive = false;
                 currentLine = 0;
-                Debug.Log("ok");                           
+                Debug.Log("ok");
+                StartCoroutine(end());
             }
-           
-            speakertext.text = convo.GetLineByIndex(currentLine).speaker.GetName(); //set speker name
-            dText.text = convo.GetLineByIndex(currentLine).dialogue; //set dialogue
-            speakerimage.sprite = convo.GetLineByIndex(currentLine).speaker.GetImage(); //set speaker image                      
+            else
+            {
+                speakertext.text = convo.GetLineByIndex(currentLine).speaker.GetName(); //set speker name
+                dText.text = convo.GetLineByIndex(currentLine).dialogue; //set dialogue
+                speakerimage.sprite = convo.GetLineByIndex(currentLine).speaker.GetImage(); //set speaker image  
+            }          
+                                
         }        
         
     }
@@ -52,5 +57,15 @@ public class DialogueManager : MonoBehaviour
         speakertext.text = convo.GetLineByIndex(currentLine).speaker.GetName(); //set speker name
         dText.text = convo.GetLineByIndex(currentLine).dialogue; //set dialogue
         speakerimage.sprite = convo.GetLineByIndex(currentLine).speaker.GetImage(); //set speaker image
+    }
+
+    IEnumerator end()
+    {        
+        //am.SetTrigger("start");
+        //ps.Play();
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(convo.Getname());
     }
 }
--- Assets/scripts/map-menuscripts/levelend.cs
+++ Assets/scripts/map-menuscripts/levelend.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class levelend : MonoBehaviour
 {
     public int cutscene;
+    public Animator am;
+
+    private ParticleSystem ps;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ps = gameObject.GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -24,8 +27,10 @@ public class levelend : MonoBehaviour
     IEnumerator end()
     {
         PlayerPrefs.SetInt("cutscenetoload", cutscene);
+        am.SetTrigger("start");
+        ps.Play();
 
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(5);
 
         SceneManager.LoadScene("Cutscene-demo");
     }
